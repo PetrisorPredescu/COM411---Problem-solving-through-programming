38d44d2b75e345bba5b348449b5ba3ebd24b2a25
PetrisorPredescu <79548847+PetrisorPredescu@users.noreply.github.com>
2021-02-27 20:21:27 +0000

    Delete multiline_message.py

--- basics/output/multiline_message.py
+++
@@ -1,7 +0,0 @@
-#Display message to standard output in multiple lines of code
-print("System failure imminent!")
-print("System Reboot has been initiated...\n")
-print("...rebooting sensoring system")
-print("...rebooting output motors")
-print("...rebooting hover engine\n")
-print("System online")
