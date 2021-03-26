04b6e38e794ab9f44db172cf11fe14deb55c280d
repl.it user <replituser@example.com>
2021-03-26 11:27:43 +0000

    sorted list with user response

--- README.md
+++ README.md
@@ -2,4 +2,6 @@
 
 week 1 - inputs/Outputs and Setups
 week 2 - 
-week 3 - 
+week 3 -
+week 4 - 
+week 5 -
---
+++ W5/simple_set.py
@@ -0,0 +1,8 @@
+def observed():
+  observations = {"it may rain", "cold", "hot", "sunny spell"}
+  return observations
+
+def run():
+  print("What is the weather? :", observed())
+
+run()
