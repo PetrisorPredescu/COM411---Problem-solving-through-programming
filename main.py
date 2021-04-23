92b1200ade3c4aa7b6f2e41f18d27595798ce4e7
repl.it user <replituser@example.com>
2021-04-23 08:56:46 +0000

    added code to display information about the current working dut current working directory

--- main.py
+++ main.py
@@ -1,32 +1,6 @@
-song = '''
-Humpty Dumpty sat on a wall
-Humpty Dumpty had a great fall
-All the king's horses and all the king's men
-Couldn't put Humpty together again
-Humpty Dumpty sat on a wall
-Humpty Dumpty had a great fall
-All the king's horses and all the king's men
-Couldn't put Humpty together again
-Humpty Dumpty sat on a wall
-Humpty Dumpty had a great fall
-All the king's horses and all the king's men
-Couldn't put Humpty together again
-'''
+import os
+path = os.getcwd()
+print(f"Current working directory: {path}")
 
-lista = song.lower().replace("\"","").replace("\'","").replace(",","").split()
-
-#print(type(lista))
-
-word_dict = {}
-
-#for word in lista:
- # if word in word_dict:
- #   word_dict[word] += 1
- # else:
- #   word_dict[word]  = 1
-
-
-for item in lista:
-  word_dict[item] = word_dict.get(item,0) +1
-
-print(dict(sorted(word_dict.items(), key = lambda x: x[1]))) 
+for file in os.listdir(path):
+  print(file)
