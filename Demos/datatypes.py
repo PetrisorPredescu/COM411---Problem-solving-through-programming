9e181021b07f390d6f43aa645bdc01f763bba6e3
repl.it user <replituser@example.com>
2021-05-04 09:51:39 +0000

    subplots

--- Demos/datatypes.py
+++ Demos/datatypes.py
@@ -1,9 +1,1 @@
-print("What is your name?")
-#Variable is a container, which can store data for us in the memory (string, intiger,float,bool)
-name = input()
-print("What is yourage?")
-age = int(input())
-print("What is your bank balace?")
-balance = float(input())
-
-print("Welcome {}. You are set to be {} years old. Your bank balance is {:.2f}.".format(name, age, balance ))
+print("{operation} has started.")"
---
+++ w8/graph_xy.py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as plt
+
+x = [0, 2, 4, 6, 8, 10,12]
+y = [0, 20, 40, 60, 80, 100,-20]
+
+plt.xlabel("High values")
+plt.ylabel("y values")
+plt.plot(x, y)
+plt.show()
